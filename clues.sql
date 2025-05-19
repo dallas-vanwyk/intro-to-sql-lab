6642b20888f187553d5f673704fe779078b17a9f
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-05-18 21:27:21 -0500

    completed exercises; need to practice JOIN to make two queries into one

--- clues.sql
+++ clues.sql
@@ -25,18 +25,19 @@
  
 -- Write SQL query here
 -- SELECT name, continent, region FROM countries WHERE continent = 'Europe';
-SELECT name, continent, region, population FROM countries
+SELECT name, continent, region, population, code FROM countries
 WHERE region = 'Southern Europe'
 ORDER BY population;
 
--- Vatican City is the least populous country in southern Europe
+-- Vatican City is the least populous country in southern Europe - code VAT
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
-SELECT code, name FROM countries
-WHERE name = 'Holy See (Vatican City State)';
+-- SELECT code, name FROM countries
+-- WHERE name = 'Holy See (Vatican City State)';
+-- above query was unnecessary once 'code' was included with Clue #1 query
 
 SELECT countrycode, language, isofficial FROM countrylanguages
 WHERE countrycode = 'VAT';
@@ -65,10 +66,28 @@ WHERE code = 'SMR';
 
 -- Write SQL query here
 
+SELECT * FROM cities
+WHERE countrycode = 'SMR';
+
+-- the city is Serravalle
+
+
+
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+SELECT * from cities
+WHERE name like 'Serra%';
+
+-- the city is Sierra, country code BRA
+
+SELECT code, name FROM countries
+WHERE code = 'BRA';
+
+-- the country is Brazil
+
+
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -76,6 +95,17 @@ WHERE code = 'SMR';
 -- follow right behind you!
 
 -- Write SQL query here
+
+SELECT code, name, capital FROM countries
+where code = 'BRA';
+
+-- Brazil's capital is city 211
+
+SELECT * FROM cities
+WHERE id = 211;
+
+-- city 211 is Brasília
+
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -90,3 +120,9 @@ WHERE code = 'SMR';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+
+SELECT name, countrycode FROM cities
+WHERE population = 91084;
+
+-- Santa Monica, USA
